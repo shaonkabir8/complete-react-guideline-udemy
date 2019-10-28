737ce5e2f1acfa2b72ac3bb8c1ffaae395c1df59
unknown <shaonkabir98@gmail.com>
2019-10-27 23:54:22 -0700

    Fresh React Application for Blog App following Maximilion's Courses

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import './Blog.css';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div>
+                <section className="Posts">
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
