1c2cde689b830f30d6d6cd6a57ce2421989dde97
unknown <shaonkabir98@gmail.com>
2019-10-28 07:57:55 -0700

    Added ! Post data to server by creating a new post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -7,6 +7,18 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+
+    postDataHandler = () => {
+        // data for sending to server
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(res => console.log(res))
     }
 
     render () {
@@ -22,7 +34,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
