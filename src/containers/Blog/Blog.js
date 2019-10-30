9f6f8e2d62ff4c70e80f496a9a0b472a70cdf944
unknown <shaonkabir98@gmail.com>
2019-10-30 08:33:42 -0700

    Updated ! link[a href=''] Upgraded to [Link]

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts'
 import NewPost from '../Blog/NewPost/NewPost'
@@ -14,8 +14,16 @@ class Blog extends Component {
                     </section>
                     <nav className="Nav">
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</Link>
+                            </li>
                         </ul>
                     </nav>
                 </header>
